fee7d0a5da4e33fbe19443a5683f6389ef2761c5
Igor Baroni <igorbaroni77@gmail.com>
2024-05-28 18:15:54 -0300

    adoptables page && some improvements

---
+++ src/js/success-page.js
@@ -0,0 +1,7 @@
+document.getElementById("back-btn").addEventListener("click", () => {
+  window.location = "/index.html";
+});
+
+document.getElementById("donate-button").addEventListener("click", () => {
+  alert("donate modal");
+});
