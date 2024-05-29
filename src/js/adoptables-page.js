bca7462633fc51e93399635525d4ed2e483171e2
Igor Baroni <igorbaroni77@gmail.com>
2024-05-29 18:00:38 -0300

    Modals design and logic

--- src/js/adoptables-page.js
+++ src/js/adoptables-page.js
@@ -1,7 +1,42 @@
+// Routes
 document.getElementById("back-button").addEventListener("click", () => {
-  window.location = "/index.html";
+  window.location = "../../index.html";
 });
 
+// Donate Modal
+const donateModal = document.getElementById("donate-modal");
+
 document.getElementById("donate-button").addEventListener("click", () => {
-    alert('donate modal')
+  donateModal.showModal();
+
+  document.getElementById("donate-email-input").blur();
+
+  donateModal.classList.add("show");
+  donateModal.classList.remove("hide");
+});
+
+document.getElementById("cancel-button").addEventListener("click", () => {
+  donateModal.classList.remove("show");
+  donateModal.classList.add("hide");
+
+  setTimeout(() => {
+    donateModal.close();
+  }, 250);
+});
+
+document.getElementById("help-button").addEventListener("click", () => {
+  window.location = "success-page.html";
+});
+
+const paymentMethods = document.querySelectorAll(".payment-method");
+
+paymentMethods[0].classList.add("selected");
+
+paymentMethods.forEach((option) => {
+  option.addEventListener("click", () => {
+    for (let i = 0; i < paymentMethods.length; i++) {
+      paymentMethods[i].classList.remove("selected");
+    }
+    option.classList.add("selected");
+  });
 });
---
+++ src/js/desktop-page.js
@@ -0,0 +1,62 @@
+// Donate Modal
+const donateModal = document.getElementById("donate-modal");
+
+document.getElementById("donate-button").addEventListener("click", () => {
+  donateModal.showModal();
+
+  document.getElementById("donate-email-input").blur();
+
+  donateModal.classList.add("show");
+  donateModal.classList.remove("hide");
+});
+
+document.getElementById("cancel-button").addEventListener("click", () => {
+  donateModal.classList.remove("show");
+  donateModal.classList.add("hide");
+
+  setTimeout(() => {
+    donateModal.close();
+  }, 250);
+});
+
+document.getElementById("help-button").addEventListener("click", () => {
+  window.location = "success-page.html";
+});
+
+const paymentMethods = document.querySelectorAll(".payment-method");
+
+paymentMethods[0].classList.add("selected");
+
+paymentMethods.forEach((option) => {
+  option.addEventListener("click", () => {
+    for (let i = 0; i < paymentMethods.length; i++) {
+      paymentMethods[i].classList.remove("selected");
+    }
+    option.classList.add("selected");
+  });
+});
+
+// Adopt Modal
+const adoptModal = document.getElementById("adopt-modal");
+
+document.getElementById("apply-button").addEventListener("click", () => {
+  adoptModal.showModal();
+
+  document.getElementById("adopt-email-input").blur();
+
+  adoptModal.classList.add("show");
+  adoptModal.classList.remove("hide");
+});
+
+document.getElementById("adopt-cancel-button").addEventListener("click", () => {
+  adoptModal.classList.remove("show");
+  adoptModal.classList.add("hide");
+
+  setTimeout(() => {
+    adoptModal.close();
+  }, 250);
+});
+
+document.getElementById("adopt-button").addEventListener("click", () => {
+  window.location = "success-page.html";
+});
